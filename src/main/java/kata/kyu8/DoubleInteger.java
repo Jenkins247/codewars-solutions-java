a726eab66b5ba883decca90720e54a19cc399e04
Jenkins247 <ealferyev@gmail.com>
2024-12-01 21:51:41 +0500

    solution DoubleInteger(8kyu)

---
+++ src/main/java/kata/kyu8/DoubleInteger.java
@@ -0,0 +1,10 @@
+package kata.kyu8;
+
+/**
+ * Code as fast as you can! You need to double the integer and return it.
+ */
+public class DoubleInteger {
+    public static int doubleInteger(int i) {
+        return i*2;
+    }
+}
---
+++ src/test/java/kata/kyu8/DoubleIntegerTest.java
@@ -0,0 +1,13 @@
+package kata.kyu8;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class DoubleIntegerTest {
+
+    @Test
+    public final void doubleIntegerTest() {
+        Assertions.assertEquals( 4, DoubleInteger.doubleInteger(2)); // Test cases here!
+    }
+}
+
